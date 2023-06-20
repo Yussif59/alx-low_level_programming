fb277d13bd1f20a66dfa120e7b9e4e34dad280d8
Yussif59 <yussif.59@gmail.com>
2023-06-20 10:52:54 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _isalpha(int c);
+int print_sing(int n);
+int _aba(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int n);
+
+#endif
