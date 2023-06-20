f205a09c566d064eb4ecaa0d0f77b2246b971ad7
Yussif59 <yussif.59@gmail.com>
2023-06-20 12:57:55 -0700

    3-islower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,10 +4,12 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 int _isalpha(int c);
-int print_sing(int n);
 int _aba(int);
+int print_sing(int n);
 int print_last_digit(int);
+void times_table(void);
 void jack_bauer(void);
 int add(int, int);
 void print_to_98(int n);
