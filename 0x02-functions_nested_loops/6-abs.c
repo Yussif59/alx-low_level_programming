fcb45f8c324d978fb61309cf5de6fd98ca9155e4
Yussif59 <yussif.59@gmail.com>
2023-06-20 15:32:15 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,19 +2,14 @@
 
 /**
  * _abs - Computes the absolute value of an integer.
- * @n: The number to be computed.
- * Return: Absolute value ofnumber or zero
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
  */
-
-int _abs(int c)
-
+int _abs(int n)
 {
-	if (c < 0)
-	{
-		int abs_val
-			;
-		abs_val = c * -1;
-		return (abs_val);
-	}
-		return (c);
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
 }
