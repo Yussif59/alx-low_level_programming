e22541bc8814f58a6cfea860c5e72ca31c2482e7
Yussif59 <yussif.59@gmail.com>
2023-07-07 05:55:46 -0700

    taski 0

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
+ * _islower - Checks for lowercase character
+ * @c: The character to be checked.
+ * Return: 1 for lowercase character or 0 for anything else
  */
+
 int _islower(int c)
+
 {
-	return (c >= 'a' && c <= 'z');
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
 }
--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
+ * _isalpha - Checks for alphabetic character
+ * @c: The character to be checked
+ * Return: 1 for alphabetic character or 0 for anything else
  */
+
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+
+	if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+		return (0);
 }
